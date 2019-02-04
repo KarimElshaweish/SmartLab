791d97e12ac9a1c6e32054ab3ef1f381bae4ff01
Karim <KarimAshrafElshaweish@outlook.com>
2019-02-04 18:59:55 +0200

    first commit

---
+++ app/src/main/java/automation/ebtkarat/myapplication/Activites/HomeActivity.java
@@ -0,0 +1,15 @@
+package automation.ebtkarat.myapplication.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import automation.ebtkarat.myapplication.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
